6f42c71a5e9546a094ac426d2ba52df9bf68fc93
Arik Kusuma <arikusumawardana09@gmail.com>
2024-01-28 16:28:28 +0800

    update file

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,16 +52,105 @@ function popup() {
   })
 }
 
-// Variable yang berisi link API
-const APIUrl = "https://lk21-api.cyclic.app";
+// Variable yang berisi link API dan API KEY
+const API_URL = "https://api.themoviedb.org/3";
+const API_KEY = "95ad9b662a7ad6aff36991bdfe9270f5";
+const BACKDROP_URL = "https://image.tmdb.org/t/p/original";
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-async function displayLatestMovie() {
-  const response = await fetch(APIUrl + "/movies", { mode: 'no-cors'});
-  const latestMovie = await response.json();
 
-  console.log('tes')
-  console.log(latestMovie);
+// Fungsi untuk menampilkan Top Rated Movies 
+async function getTopRatedMovies() {
+  const response = await fetch(API_URL + "/movie/top_rated?api_key=" + API_KEY);
+  const data = await response.json();
+  const topRatedMovies = data.results.slice(0, 3) // mengambil 3 movie top rated
+  
+  console.log(topRatedMovies);
+  
+  const topRatedId = document.getElementById('topRated');
+  topRatedId.innerHTML = '';
+
+  topRatedMovies.forEach(movie => {
+    const {id, title, backdrop_path, vote_average} = movie;
+    const topRatedEl = document.createElement('div');
+    topRatedEl.classList.add('swiper-slide', 'swiper-img');
+    topRatedEl.innerHTML = `
+        <img src="${BACKDROP_URL + backdrop_path}" alt="${title}">
+        <div class="swiper-text">
+            <span>${vote_average}</span>
+            <h1>${title}</h1>
+            <a href="#" class="btn-main">See Detail</a>
+        </div>
+    `;
+
+    topRatedId.appendChild(topRatedEl);
+
+  });
 
 }
 
-displayLatestMovie()
+// Memanggil Fungsi getTopRatedMovies
+getTopRatedMovies();
+
+
+// Fungsi untuk menampilkan Popular Movies
+async function getPopularMovies() {
+  const response = await fetch(API_URL + "/movie/popular?api_key=" + API_KEY);
+  const data = await response.json();
+  const popularMovies = data.results.slice(0, 10) // mengambil 10 movie popular
+  
+  const popularId = document.getElementById('popular');
+  popularId.innerHTML = '';
+
+  popularMovies.forEach(movie => {
+    const {id, title, poster_path, release_date, vote_average} = movie;
+    const popularEl = document.createElement('div');
+    popularEl.classList.add('card-main', 'swiper-slide');
+    popularEl.innerHTML = `
+      <a href="" class="link-movies">
+          <div class="card-img">
+              <img src="${IMG_URL + poster_path}" alt="${title}">
+          </div>
+          <h3>${title}</h3>
+          <span>${release_date}</span>
+      </a>
+    `;
+
+    popularId.appendChild(popularEl);
+    
+  });
+}
+
+// Memanggil Fungsi getPopularMovies
+getPopularMovies();
+
+
+async function getUpcomingMovies() {
+  const response = await fetch(API_URL + "/movie/upcoming?api_key=" + API_KEY);
+  const data = await response.json();
+  const upcomingMovies = data.results.slice(0, 10) // mengambil 10 upcoming movies 
+
+  const upcomingId = document.getElementById('upcomingMovies');
+  upcomingId.innerHTML = '';
+
+  upcomingMovies.forEach(movie => {
+    const {id, title, poster_path, release_date, vote_average} = movie;
+    const upcomingEl = document.createElement('div');
+    upcomingEl.classList.add('card-main');
+    upcomingEl.innerHTML = `
+      <a href="" class="link-movies">
+        <div class="card-img new-img">
+            <img src="${IMG_URL + poster_path}" alt="${title}">
+        </div>
+        <h3>${title}</h3>
+        <span>${release_date}</span>
+      </a>
+    `;
+
+    upcomingId.appendChild(upcomingEl);
+
+  });
+}
+
+// Memanggil fungsi getUpcomingMovies
+getUpcomingMovies();
