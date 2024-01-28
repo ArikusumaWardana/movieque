95d88ff19fbaad12d4ca1e83fae397a9bf8126db
Arik Kusuma <arikusumawardana09@gmail.com>
2024-01-28 13:55:53 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+// Scroll Y Navbar  
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+  header.classList.toggle('bg-nav', window.scrollY > 0 );
+});
+
+// Swiper Carousel
+var swiper = new Swiper(".jumbotron", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+
+// Swiper Popular
+var swiper = new Swiper(".popular-container", {
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+    delay:3000,
+    disableOnInteraction:false,
+  },
+  centeredSlides:true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    568: {
+      slidesPerView: 3,
+    },
+    768:  {
+      slidesPerView: 4,
+    },
+    968: {
+      slidesPerView:5
+    },
+  },
+});
+
+
+function popup() {
+  const dropdown = document.querySelector('#dropdown-main')
+  this.addEventListener('click', function() {
+    dropdown.classList.toggle('dropdown-none')
+  })
+}
+
+// Variable yang berisi link API
+const APIUrl = "https://lk21-api.cyclic.app";
+
+async function displayLatestMovie() {
+  const response = await fetch(APIUrl + "/movies", { mode: 'no-cors'});
+  const latestMovie = await response.json();
+
+  console.log('tes')
+  console.log(latestMovie);
+
+}
+
+displayLatestMovie()
